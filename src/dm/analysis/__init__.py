6d68f490d69a7ead4b56a9cf76a4d25b59a32cf1
Dalong Wu <wdl200268@gmail.com>
2026-04-28 09:00:21 +0800

    Export logSNR analysis helper

--- src/dm/analysis/__init__.py
+++ src/dm/analysis/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from dm.analysis.mechanism import (
     BandSpec,
     fft_radial_band_energy,
+    logsnr_bin_edges,
     logsnr_bin_centers,
     logsnr_bin_index,
     radial_band_spec,
@@ -11,6 +12,7 @@ from dm.analysis.mechanism import (
 __all__ = [
     "BandSpec",
     "fft_radial_band_energy",
+    "logsnr_bin_edges",
     "logsnr_bin_centers",
     "logsnr_bin_index",
     "radial_band_spec",
